a95c9ecbb9ad973108e288a7e5fe85723148f897
aviv0404 <aviv040400@gmail.com>
2017-02-24 00:30:21 +0200

    Update SpecialStrategy.java

--- bots/SpecialStrategy.java
+++ bots/SpecialStrategy.java
@@ -1,7 +1,12 @@
 package bots;
 
-import pirates.*;
-import java.util.List;
+import java.util.*
+import pirates.game.*
+import java.lang.Math
+import java.lang.reflect
+import java.lang.class
+import java.math
+
 
 class SpecialStrategy implements Strategy {
 
