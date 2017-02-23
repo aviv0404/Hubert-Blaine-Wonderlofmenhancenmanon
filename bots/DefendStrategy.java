22293117cffcdf5afa40d9d8d709f5566528f0af
aviv0404 <aviv040400@gmail.com>
2017-02-24 00:29:19 +0200

    Update DefendStrategy.java

--- bots/DefendStrategy.java
+++ bots/DefendStrategy.java
@@ -1,7 +1,12 @@
 package bots;
 
-import java.util.List;
-import pirates.*;
+import java.util.*
+import pirates.game.*
+import java.lang.Math
+import java.lang.reflect
+import java.lang.class
+import java.math
+
 
 class DefendStrategy implements Strategy {
 
