b9efdf6a2c1edb6e84ac35e85b3d2ae1b19e5340
aviv0404 <aviv040400@gmail.com>
2017-02-24 00:23:38 +0200

    Update DefendStrategy.java

--- bots/DefendStrategy.java
+++ bots/DefendStrategy.java
@@ -1,7 +1,28 @@
 package bots;
+
+import java.util.List;
 import pirates.*;
 
 class DefendStrategy implements Strategy {
-    public void doTurn(History history) {
+
+    
+    //TODO: mabye change myCities.get(0) if there's more than one city
+    @Override
+    public void doTurn(PirateGame game, History history) {
+        game.debug("Activated: DefendStrategy");
+        List<Pirate> myLivingPirates = game.getMyLivingPirates();
+        List<Drone> enemyLivingDrones = game.getEnemyLivingDrones();
+        List<City> enemyCities = game.getEnemyCities();
+        int numMyLivingPirates = myLivingPirates.size();
+        game.debug(numMyLivingPirates);
+        for (int i = 0; i < numMyLivingPirates; i++) {
+            if (!Attacker.tryAttack(myLivingPirates.get(i), game)) {
+                Mover.moveAircraftToClosestToAnotherMapObject(
+                                myLivingPirates.get(i),
+                                enemyLivingDrones,
+                                enemyCities.get(0),
+                                game);
+            }
+        }
     }
 }
