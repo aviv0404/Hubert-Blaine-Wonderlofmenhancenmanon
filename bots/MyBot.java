371bae0ef43791a8ed964757ca1d168c40d106a8
aviv0404 <aviv040400@gmail.com>
2017-03-07 18:04:16 +0200

    Update MyBot.java

--- bots/MyBot.java
+++ bots/MyBot.java
@@ -1,14 +1,12 @@
 package bots;
 import pirates.*;
 import java.util.List;
-
 /**
  * Agada bot :)
  */
 public class MyBot implements PirateBot {
     private History history;
     private Strategy strategy;
-
     @Override
     public void doTurn(PirateGame game) {
         
@@ -20,15 +18,12 @@ public class MyBot implements PirateBot {
         strategy.doTurn(game,history);
         
     }
-
     private boolean firstTurn(PirateGame game) {
         return game.getTurn() == 1;
     }
-
     private void initFirstTurn() {
         history = new History();
     }
-
     private Strategy decideStrategy(PirateGame game) {
         if (game.getEnemyCities().size() == 0 && game.getMyCities().size() != 0){
             return new AttackStrategy();
